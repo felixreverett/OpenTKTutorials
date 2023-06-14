261f0a18f105c251c44c227daf5f5f2d6a741ed0
felixreverett <felixreverett@gmail.com>
2023-06-14 14:12:58 +0100

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTKTutorials
+{
+    public class Game
+    {
+    }
+}
